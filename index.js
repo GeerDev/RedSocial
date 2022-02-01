03e0b9347065fbd9858f231c5d0f99135df799f9
German <gerfersie@gmail.com>
2022-02-01 14:00:53 +0100

    Conectando la base de datos y comprobando su funcionamiento

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+require('dotenv').config()
+
+const { dbConnection } = require('./database/config')
+const PORT = process.env.PORT
+
+app.use(express.json())
+
+dbConnection()
+
+// app.use('/users', require('./routes/users'));
+
+
+app.listen(PORT, () => console.log(`Servidor levantado en el puerto ${PORT}`))
