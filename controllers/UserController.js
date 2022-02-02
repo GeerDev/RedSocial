849161ac472d6f0375efbf4e8a67cb85cb5f93be
German <gerfersie@gmail.com>
2022-02-02 09:07:45 +0100

    Login de Usuario

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -25,16 +25,35 @@ const UserController = {
     },
     async login(req, res) {
         try {
-            const user = await User.findOne({
-                name: req.body.name
-            }) 
+            if(!req.body.password || !req.body.email){
+                return res.status(400).json({msg:'Por favor rellene los campos que faltan'})
+            }
 
-            res.send({user});
+            const user = await User.findOne({
+                email: req.body.email
+            }) 
+    
+            if (!user) {
+                return res.status(400).send({ message: 'Contraseña o nombre incorrectos' });
+            }
+            const isMatch = await bcrypt.compare(req.body.password, user.password)
+
+            if (!isMatch) {
+                return res.status(400).send({ message: 'Contraseña o nombre incorrectos' });
+            }
+            
+            token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+            if (user.tokens.length > 3) user.tokens.shift();
+            user.tokens.push(token);
+            await user.save();
+
+            res.send({ message: 'Bienvenid@ ' + user.name, token });
 
         } catch (error) {
             console.error(error);
+            res.status(500).send({ error, message: 'Hubo un problema al tratar de hacer el logeo' })
         }
-    }
+    },
 }
 
 module.exports = UserController
